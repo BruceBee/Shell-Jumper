99161663ae381765b556b2964ff30a5c0842f51a
Bruce Bee <mzpy_1119@126.com>
2020-06-17 11:51:50 +0800

    Update jumper.sh

--- data/jumper.sh
+++ data/jumper.sh
@@ -17,6 +17,7 @@ function loadHost(){
             let hostCount+=1
         fi
     done < $jumperList
+#    echo ${tmps[@]}
 }
 
 # 加载用户登陆IP权限
@@ -29,20 +30,18 @@ function loadPermission(){
         if [[ "$line" =~ ^"$1".* ]]; then
             arry=($line)
             IFS="$OLD_IFS"
+	    #echo ${arry[$*]}
             for var in ${arry[1]}
             do
                 IFS=","
-		arry2=($var)
+	        arry2=($var)
 		for var2 in ${arry2[@]}
-	        do
+		do
 		    tmpsPer[$permissionCount]=$var2
 		    let permissionCount+=1
+		   
 		done
             done
-	    
-            #permission=(${"$line"//=/})
-            #echo $permission
-
 	fi
     done < $jumperPermission
 }
@@ -55,7 +54,7 @@ function compare(){
     do
         for j in ${tmps[@]}
 	do
-            if [[ "$j" =~ ^"$i".* ]]; then
+            if [[ "${j%%=*}" == "$i" ]]; then
                 dd[$cc]=$j
                 let cc+=1
             fi
@@ -74,14 +73,20 @@ function trapper(){
 function menu(){
     ff=0
 
-    echo "========= `whoami`, 欢迎登陆跳板机=========="	
+    echo -e "=========\033[44;37m `whoami` \033[0m, 欢迎您登录跳板机=========="	
+    echo -e "现在是北京时间: " "\033[32m $(date "+%F %T") \033[0m"
     for d in ${dd[@]}
     do
-	echo "["$ff"]" $d
-	let ff+=1
+   
+	    echo -e "\033[32m ["$ff"] \033[0m" `echo $d | cut -d"=" -f1`  "\t\t\t"  
+	    let ff+=1
+      
     done
-    echo "[99] 退出跳板机"
-    echo "============================================"
+    wait
+    echo -e "\033[31m [99]\033[0m  退出跳板机 "
+    echo -e "\033[35m [111] 查询服务所在机器 \033[0m"
+    echo -e "\033[31m 友情提示: 所有主机默认不操作300s即自动断开 \033[0m"
+    echo -e "================================================"
 }
 
 
@@ -124,6 +129,7 @@ function host(){
 		let remoteIdx+=1
             done
 	done
+	wait
         user=`whoami`
         echo  "${remoteHost[0]} ${remoteHost[1]} $user"
         
@@ -140,16 +146,22 @@ function main(){
   loadPermission `whoami`
   loadHost
   compare
+  error=1
   while true
   do
     trapper
     clear
     menu
     read -p "请输入您要登陆的主机序号:" num
+    
+    if [[ $num == 111 ]]; then
+        less /tmp/services.txt
+    fi
 
     if [[ $num == 99 ]]; then
         break;
     else
+        [ $error -gt 3 ] && echo "$(whoami)你好,请输入数字范围,否则你只能在这里, 谢谢合作 ~@ @~"
         res=$(host "$num")
 	if [[ $res != 1 ]]; then
 
@@ -157,15 +169,22 @@ function main(){
 	    OLD_IFS="$IFS"
 	    IFS=" "
 	    arry=($res IFS="$OLD_IFS")
-
+           
+	    echo ${arry[2]} $(date "+%F %T") ${arry[0]} "login" >> /usr/local/records/login.txt;
+	   
+	    error=0 
 	    # 执行expect
-            exec /usr/bin/expect /data/ex.sh $res
+            /usr/bin/expect /data/ex.sh $res
+	    echo ${arry[2]} $(date "+%F %T") ${arry[0]} "logout" >> /usr/local/records/login.txt; 
+
         else
 	    echo "[x]请输入正确选项"
 	    sleep 1;
 	fi
 
     fi	
+    let error+=1
+    sleep 0.1
   done
 }
 
